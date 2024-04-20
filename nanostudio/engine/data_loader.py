84572cf049a3a549735d6cf5f30aaaf8f5526c1a
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2024-04-20 22:43:35 +0530

    Update: fix dataloader and model classes

--- nanostudio/engine/data_loader.py
+++ nanostudio/engine/data_loader.py
@@ -3,38 +3,29 @@ import torch
 import os
 
 class DataLoader:
-    def __init__(self, data_dir='data', block_size = 1024, batch_size = 12, device='cpu'):
+    def __init__(self, data_dir='data', block_size=1024, batch_size=12, device='cpu'):
         self.data_dir = data_dir
         self.block_size = block_size
         self.batch_size = batch_size
         self.device = device
+        self.data_cache = {}
 
     def load_data(self, split):
-        """
-        Load data from a numpy memmap file based on the split.
-        """
-        file_path = os.path.join(self.data_dir, f'{split}.bin')
-        return np.memmap(file_path, dtype=np.uint16, mode='r')
+        if split not in self.data_cache:
+            file_path = os.path.join(self.data_dir, f'{split}.bin')
+            data_array = np.memmap(file_path, dtype=np.uint16, mode='r')
+            if len(data_array) < self.block_size:
+                raise ValueError("Dataset is too small for the defined block_size.")
+            self.data_cache[split] = data_array
+        return self.data_cache[split]
 
-    def get_batch(self, data):
-        """
-        Randomly sample batch indices and extract sequences from the data array.
-        """
-        # Calculate the number of possible sequences based on data length and block size
-        num_sequences = len(data) - self.block_size
-        # Randomly sample indices for batch sequences
-        indices = torch.randint(0, num_sequences, (self.batch_size,))
-        # Extract sequences from the data array
-        x = torch.stack([torch.from_numpy(data[int(index):int(index) + self.block_size]).long() for index in indices])
-        y = torch.stack([torch.from_numpy(data[int(index) + 1:int(index) + self.block_size + 1]).long() for index in indices])
-        # Move data to the specified device, e.g., GPU or CPU
-        x = x.to(self.device, non_blocking=True)
-        y = y.to(self.device, non_blocking=True)
-        return x, y
-
-    def fetch_batch(self, split='train'):
-        """
-        Convenience method to fetch a batch from the specified split.
-        """
+    def get_batch(self, split='train'):
         data = self.load_data(split)
-        return self.get_batch(data)
+        num_sequences = len(data) - self.block_size
+        while True:
+            start_indices = np.random.randint(0, num_sequences, size=(self.batch_size,))
+            x = [data[i:i+self.block_size] for i in start_indices]
+            y = [data[i+1:i+self.block_size+1] for i in start_indices]
+            x_tensor = torch.tensor(x, dtype=torch.long).to(self.device, non_blocking=True)
+            y_tensor = torch.tensor(y, dtype=torch.long).to(self.device, non_blocking=True)
+            yield x_tensor, y_tensor
